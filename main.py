d00afeac1eca0ca6417c8810c78f8517735cd5fe
Nguyen Thanh <73066048+thanhprao@users.noreply.github.com>
2023-06-30 11:25:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , render_template
 import os
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
