9ccadf6eba63aa519bf59eeb7efa5adc92be6ef0
Nguyen Thanh <73066048+thanhprao@users.noreply.github.com>
2023-06-30 11:50:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify , render_template
+from flask import Flask, jsonify, render_template, request
 import os
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,6 +10,17 @@ app = Flask(__name__, template_folder='Template')
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+@app.get("/homepage")
+async def demo_get():
+    selenium_code()
+
+def selenium_code():
+    chrome_option = webdriver.ChromeOptions()
+    # chrome_option.add_argument("--user-data-dir="+accounts)
+    # chrome_option.add_argument("--profile-directory="+profile)
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s,options=chrome_option)
+    driver.maximize_window()
+    driver.get("https://google.com")   
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
