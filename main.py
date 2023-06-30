dc06f4a433ad11b5b0102bb31a687c700d787664
Nguyen Thanh <73066048+thanhprao@users.noreply.github.com>
2023-06-30 11:29:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,17 @@ app = Flask(__name__, template_folder='Template')
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.get("/homepage")
+async def demo_get():
+    selenium_code()
 
+def selenium_code():
+    chrome_option = webdriver.ChromeOptions()
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s,options=chrome_option)
+    driver.maximize_window()
+    driver.get("https://google.com")
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
