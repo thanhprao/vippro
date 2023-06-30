5c04b2583265c6c6a1d3c46249377fa8b84e8188
Nguyen Thanh <73066048+thanhprao@users.noreply.github.com>
2023-06-30 10:55:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 import os
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Template')
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
